5f431fab0abbeb77364e43d12bfbf2f9bc585309
Ethan Feldman <ejfeldman7@gmail.com>
2024-01-01 08:55:37 -0800

    Reorg files and import

---
+++ scraping_functions.py
@@ -0,0 +1,104 @@
+
+import pandas as pd
+import numpy as np
+import re
+from IPython.core.display import display, HTML
+from bs4 import BeautifulSoup
+import requests
+import seaborn as sns
+import matplotlib.pyplot as plt
+from urllib.request import Request, urlopen
+import pickle
+from collections import defaultdict
+
+
+def get_urls():
+    # URL for specific Coffee
+    search = 'https://www.coffeereview.com/advanced-search/'
+    req = Request(search, headers={'User-Agent': 'Mozilla/5.0'})
+
+    # Read into a soup variable
+    webpage = urlopen(req).read()
+    searchsoup = BeautifulSoup(webpage, 'html')
+
+    tables = searchsoup.find_all('div',attrs={'class':'review-template'})
+
+    url_list =[]
+    for i in range(len(tables)):
+        url_list = url_list + ['https://www.coffeereview.com'+ tables[i].find('div',attrs={'class':'row row-3'}).find('a').get('href')]
+
+    for i in range(2,299):
+        temp_search = 'https://www.coffeereview.com/advanced-search/?pg='+str(i)
+        temp_req = Request(temp_search, headers={'User-Agent': 'Mozilla/5.0'})
+
+        # Read into a soup variable
+        temp_webpage = urlopen(temp_req).read()
+        temp_soup = BeautifulSoup(temp_webpage, 'html')
+
+        temp_tables = temp_soup.find_all('div',attrs={'class':'review-template'})
+
+        for j in range(len(temp_tables)):
+            url_list = url_list + ['https://www.coffeereview.com'+ temp_tables[j].find('div',attrs={'class':'row row-3'}).find('a').get('href')]
+    return url_list
+
+
+def get_coffee(url_list): 
+    coffee = pd.DataFrame()
+    for link in url_list:
+        req = Request(link, headers={'User-Agent': 'Mozilla/5.0'})
+
+        # Read into a soup variable
+        webpage = urlopen(req).read()
+        soup = BeautifulSoup(webpage, 'html')
+        try:
+            roaster = soup.find('div',attrs = {'class': 'review-template'}).find_all('p')[0].text
+        except AttributeError:
+            roaster = ''
+        try:
+            review = soup.find('div',attrs = {'class': 'review-template'}).find_all('p')[1].text.partition(': ')[2]
+        except AttributeError:
+            review = ''
+        try:
+            notes = soup.find('div',attrs = {'class': 'review-template'}).find_all('p')[2].text.partition(': ')[2]
+        except AttributeError:
+            notes = ''
+        try:
+            tldr = soup.find('div',attrs = {'class': 'review-template'}).find_all('p')[3].text.partition(': ')[2]
+        except AttributeError:
+            tldr = ''
+
+        temp_dict = defaultdict(str)
+        temp_dict[roaster] = [review,notes, tldr]
+
+        coffee = pd.concat([coffee,pd.DataFrame.from_dict(temp_dict, orient='index',columns=['Review', 'Notes', 'TLDR'])])
+    return coffee
+
+
+def get_ratings(url_list):
+    ratings = pd.DataFrame()
+    for link in url_list:
+        req = Request(link, headers={'User-Agent': 'Mozilla/5.0'})
+
+        # Read into a soup variable
+        webpage = urlopen(req).read()
+        soup = BeautifulSoup(webpage, 'html')
+        try:
+            roaster = soup.find('div',attrs = {'class': 'review-template'}).find_all('p')[0].text
+        except AttributeError:
+            roaster = ''
+
+        # Scrape the categories and values for each
+        # Overall score
+        num = soup.find('div', attrs={'class':'column col-1'}).find('span', attrs={'class':'review-template-rating'}).text
+        # Categories
+        categories = soup.find_all('table',attrs = {'class':'review-template-table'})[0].find_all('td')[0::2]+soup.find_all('table',attrs = {'class':'review-template-table'})[1].find_all('td')[0::2]
+        # Scores and values
+        scores = soup.find_all('table',attrs = {'class':'review-template-table'})[0].find_all('td')[1::2]+soup.find_all('table',attrs = {'class':'review-template-table'})[1].find_all('td')[1::2]
+        y = ['Overall']+[cat.get_text().partition(':')[0] for cat in categories]
+        x = [num]+[score.get_text() for score in scores]
+
+        temp_dict = defaultdict(str)
+        temp_dict[roaster] = x
+
+        ratings = pd.concat([ratings,pd.DataFrame.from_dict(temp_dict, orient='index',columns=y)])
+    return ratings
--- web_app/web_app.py
+++ web_app/web_app.py
@@ -1,132 +1,76 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# ## Streamlit Application
-# 
-# Streamlit application for deploying coffee recommender, score predictor, and review generator.
-
-# In[8]:
-
-
-# !pip install pipreqsnb
-# !pipreqsnb C:\Users\ejfel\Documents\metis_repos\Coffee-Reviews-NLP\coffee.py
-
-
-# In[2]:
-
-
-import streamlit as st
-
-import pandas as pd
-pd.set_option('display.max_columns', None)
-import numpy as np
 import re
 import requests
 import pickle
 from collections import defaultdict
 import random
+import os.path
 
-import nltk
-nltk.download('vader_lexicon')
-from nltk.sentiment.vader import SentimentIntensityAnalyzer
-from nltk.tokenize import word_tokenize
+import streamlit as st
+import pandas as pd
+import numpy as np
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.decomposition import NMF
 from sklearn.metrics import pairwise_distances
-
-import plotly
 import plotly.graph_objects as go
 import plotly.express as px
-from plotly.subplots import make_subplots
+import nltk
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
+nltk.download('vader_lexicon')
 
-#Paths
-#'/app/Coffee-Reviews-NLP/web_app/models_embeddings/'
-#'/app/Coffee-Reviews-NLP/web_app/data/'
 
-with open('/app/coffee-reviews-nlp/web_app/coffee_words.pickle','rb') as read_file:
-    coffee = pickle.load(read_file)
-with open('/app/coffee-reviews-nlp/web_app/coffee_ratings.pickle','rb') as read_file:
-    ratings = pickle.load(read_file)
-with open('/app/coffee-reviews-nlp/web_app/combined.pickle','rb') as read_file:
-    combined = pickle.load(read_file)
-with open('/app/coffee-reviews-nlp/web_app/df_full.pickle','rb') as read_file:
-    df = pickle.load(read_file)
-with open('/app/coffee-reviews-nlp/web_app/df_topic_breakdown.pickle','rb') as read_file:
-    df_topic_breakdown = pickle.load(read_file)
-with open('/app/coffee-reviews-nlp/web_app/sentiment.pickle','rb') as read_file:
-    sentiment = pickle.load(read_file)
-with open('/app/coffee-reviews-nlp/web_app/generating_reviews.pickle','rb') as read_file:
-    generating_reviews = pickle.load(read_file)
+pickle_jar = '/app/Coffee-Reviews-NLP/web_app/models_embeddings/'
+data_path = '/app/Coffee-Reviews-NLP/web_app/data/'
+data = {'df_full': None, 'df_topic_breakdown': None, 'coffee_ratings': None}
+pickles = {'coffee_words': None, 'lm_acidity': None, 'lm_aftertaste': None
+           , 'lm_flavor': None, 'lm_body': None, 'lm_aroma': None
+           , 'generating_reviews': None, 'blindtfidf_vec': None
+           , 'nmf_tfidfblind': None, 'blindvectorizer': None
+           , 'blindtfidf_topic': None, 'num_to_score_RF': None}
 
-with open('/app/coffee-reviews-nlp/web_app/blindtfidf_vec.pickle', 'rb') as read_file:
-    blindtfidf = pickle.load(read_file)
-with open('/app/coffee-reviews-nlp/web_app/blindtfidf_mat.pickle', 'rb') as read_file:
-    tfidf_blind = pickle.load(read_file)
+for pkl, var in pickles.itmes():
+    with open(f'{pickle_jar}{pkl}.pickle', 'rb') as f:
+        pickles[pkl] = pickle.load(f)
+
+for datum, var in data.items():
+    with open(f'{data_path}{datum}.pickle', 'rb') as f:
+        data[datum] = pickle.load(f)
+
+doc_topic = pickles['blindtfidf_topic']
+blindtfidf = pickles['blindtfidf_vec']
+nmf_tfidfblind = pickles['nmf_tfidfblind']
+blindvectorizer = pickles['blindvectorizer']
+rfr_num = pickles['num_to_score_RF']
+
+lm = pickles['lm']
+lm_aroma = pickles['lm_aroma']
+lm_acidity = pickles['lm_acidty']
+lm_aftertaste = pickles['lm_aftertaste']
+lm_flavor = pickles['lm_flavor']
+lm_body = pickles['lm_body']
+generating_reviews = pickles['generating_reviews']
+
+ratings = data['coffee_ratings']
+df_full = data['df_full']
+df_topic_breakdown = data['df_topic_breakdown']
+
 ratings = ratings.reset_index().rename(columns={'index':'Roaster'})
-
-with open('/app/coffee-reviews-nlp/web_app/nmf_tfidfblind.pickle', 'rb') as read_file:
-    nmf_tfidfblind = pickle.load(read_file)
-
-with open('/app/coffee-reviews-nlp/web_app/blindvectorizer.pickle', 'rb') as read_file:
-    blindvectorizer = pickle.load(read_file)
-with open('/app/coffee-reviews-nlp/web_app/blindtfidf_topic.pickle', 'rb') as read_file:
-    blindtfidf_topic = pickle.load(read_file)
-with open('/app/coffee-reviews-nlp/web_app/blindtopic_tfidf.pickle', 'rb') as read_file:
-    blindtopic_tfidf = pickle.load(read_file)
-
-
-with open('/app/coffee-reviews-nlp/web_app/words_to_score_rf.pickle','rb') as read_file:
-    rfr = pickle.load(read_file)
-with open('/app/coffee-reviews-nlp/web_app/num_to_score_RF.pickle','rb') as read_file:
-    rfr_num = pickle.load(read_file)
-with open('/app/coffee-reviews-nlp/web_app/words_to_score_linear.pickle','rb') as read_file:
-    lm = pickle.load(read_file)
-with open('/app/coffee-reviews-nlp/web_app/subcats_to_score_lasso.pickle','rb') as read_file:
-    lasso = pickle.load(read_file)
-with open('/app/coffee-reviews-nlp/web_app/lm_aroma.pickle','rb') as read_file:
-    lm_aroma = pickle.load(read_file)
-with open('/app/coffee-reviews-nlp/web_app/lm_acidity.pickle','rb') as read_file:
-    lm_acidity = pickle.load(read_file)
-with open('/app/coffee-reviews-nlp/web_app/lm_aftertaste.pickle','rb') as read_file:
-    lm_aftertaste = pickle.load(read_file)
-with open('/app/coffee-reviews-nlp/web_app/lm_flavor.pickle','rb') as read_file:
-    lm_flavor = pickle.load(read_file)
-with open('/app/coffee-reviews-nlp/web_app/lm_body.pickle','rb') as read_file:
-    lm_body = pickle.load(read_file)
-
-import os.path
 
 # create a button in the side bar that will move to the next page/radio button choice
 next = st.sidebar.button('Next on list')
-
-# will use this list and next button to increment page, MUST BE in the SAME order
-# as the list passed to the radio button
 new_choice = ['Home','Recommender','Score from Text','Score from Subscores','Generated Reviews']
 
-# This is what makes this work, check directory for a pickled file that contains
-# the index of the page you want displayed, if it exists, then you pick up where the
-#previous run through of your Streamlit Script left off,
-# if it's the first go it's just set to 0
 if os.path.isfile('next.p'):
     next_clicked = pickle.load(open('next.p', 'rb'))
-    # check if you are at the end of the list of pages
     if next_clicked == len(new_choice):
-        next_clicked = 0 # go back to the beginning i.e. homepage
+        next_clicked = 0
 else:
-    next_clicked = 0 #the start
+    next_clicked = 0 
 
-# this is the second tricky bit, check to see if the person has clicked the
-# next button and increment our index tracker (next_clicked)
 if next:
-    #increment value to get to the next page
     next_clicked = next_clicked +1
-
-    # check if you are at the end of the list of pages again
     if next_clicked == len(new_choice):
-        next_clicked = 0 # go back to the beginning i.e. homepage
-
-# create your radio button with the index that we loaded
-choice = st.sidebar.radio("go to",('Home','Recommender','Score from Text','Score from Subscores','Generated Reviews'), index=next_clicked)
+        next_clicked = 0
+choice = st.sidebar.radio("go to", ('Home','Recommender','Score from Text','Score from Subscores','Generated Reviews'), index=next_clicked)
 
 st.sidebar.write(
     '''
@@ -138,7 +82,7 @@ st.sidebar.write(
 # pickle the index associated with the value, to keep track if the radio button has been used
 pickle.dump(new_choice.index(choice), open('next.p', 'wb'))
 
-# finally get to whats on each page
+# Define informatoin on each page
 if choice == 'Home':
     st.title('Welcome to my data analysis app for coffee reviews!')
     '''
@@ -164,7 +108,7 @@ elif choice == 'Recommender':
     st.write('Get a new coffee recommendation. Please keep in mind the reviews in this recommendation span across multiple years and the coffee recommended may not be currently available.')
     st.write('Please select from __one__ of the two options below.')
     
-    #Radio button for input style
+    # Radio button for input style
     rec_choice = st.radio('Choices',('I want to enter a description of my own',"I'd like a list of adjectives to choose from"),index=next_clicked)
 #     first = st.checkbox('I want to enter a description of my own')
 #     second = st.checkbox("I'd like a list of adjectives to choose from")
@@ -242,12 +186,12 @@ elif choice == 'Recommender':
     # Give recommendations   
     else:
         # Setting up polar plots for comparison of input and recommendation
-        example_comps=[doc_topic[recs[0]],tt1[0]]
-        names = [ratings.iloc[recs[0]]['Roaster'],'Your Input Description']
+        example_comps=[doc_topic[recs[0]], tt1[0]]
+        names = [ratings.iloc[recs[0]]['Roaster'], 'Your Input Description']
         categories = ['bright_floral_citrus', 'choc_woody_dark', 'tart_sweet_smooth','cacao_nut_clean', 'sweet_nut_pine', 'juicy_cacao_honey', 'red_berries','woody_nut_caramel', 'cherry_vinuous_choc']
         topics = ['Bright, Floral, Citrus', 'Chocolate, Dark, Woody', 'Tart, Sweet, Smooth','Cacao, Nutty, Clean', 'Sweet, Nut, Pine', 'Juicy, Honey, Cacao', 'Red Berries','Nutty, Caramel, Woody', 'Cherry, Vinuous, Chocolate']
         fig = go.Figure()
-        for i in range(0,2):
+        for i in range(0, 2):
             fig.add_trace(go.Scatterpolar(
                   r=example_comps[i],
                   theta=topics,
@@ -414,180 +358,3 @@ elif choice == 'Generated Reviews':
         st.write('Pick a coffee type or combine a few to see a (rough) computer generated review!')
     else:
         st.write(generate_review(markov_chain(text_list)))
-
-    
-
-
-# In[ ]:
-
-
-# st.title('Coffee Review Recommendations and Analysis')    
-
-# # st.write("Using a radio button restricts selection to only one option at a time.")
-# # neighborhood = st.radio("Neighborhood", df.neighbourhood_group.unique())
-# # show_exp = st.checkbox("Include expensive listings")
-# # Set up model
-# doc_word = tfidf_blind
-
-# nmf_model = nmf_tfidfblind
-# doc_topic = blindtfidf_topic
-# topic_word = nmf_model.components_
-
-# words = blindtfidf.get_feature_names()
-# t = nmf_model.components_.argsort(axis=1)[:,-1:-7:-1]
-
-# topic_words = [[words[e] for e in l] for l in t]
-
-# # First checkbox: coffee recommender
-
-# if st.checkbox('Get a recommendation for coffee from a description'):
-#     # Format inputs
-#     user_coffee_description = st.text_input("Give a couple sentences here of how you describe your ideal coffee. Try to include as much as you can about your desired flavor profile.", '')
-#     text = [user_coffee_description]
-#     vt = blindtfidf.transform(text).todense()
-#     tt1 = nmf_model.transform(vt)
-
-#     #Find Recommendations
-#     indices = pairwise_distances(tt1.reshape(1,-1),doc_topic,metric='cosine').argsort()
-#     recs = list(indices[0][0:4])
-#     # df_topic_breakdown.iloc[recs]
-#     # st.write('The coffee you liked was described as:',t[0])
-#     st.write('\n')
-#     if user_coffee_description == '':
-#         st.write('Excited to recommend a coffee for you!')
-#     else:
-#         st.write('Based on your input coffee, I recommend you try:','\n\n',ratings.iloc[recs[0]]['Roaster'],'who roast a bean from',ratings.iloc[recs[0]]['Coffee Origin'],'.','\n\n','It could be desribed as:','\n\n',coffee.iloc[recs[0]].Review)
-
-# # Second checkbox: coffee scorer
-# if st.checkbox('Predict overall and category score predictions for a coffee description'):
-#     user_coffee_description = st.text_input("Provide a couple sentence descripton of the flavors, acid level, aroma, aftertaste, and body of your coffee.", '')
-#     user_text = [user_coffee_description]
-#     vt = blindtfidf.transform(user_text).todense()
-#     tt1 = nmf_model.transform(vt)
-    
-#     word_count = pd.DataFrame()
-#     word_count['text'] = user_text
-#     word_count['length'] = word_count.text.str.replace(r'\d+','',regex=True).str.len()
-#     word_count['word count'] = pd.DataFrame(blindvectorizer.transform(user_text).toarray()).sum(axis=1)[0]
-#     word_count.drop(columns='text',inplace=True)
-    
-#     sid = SentimentIntensityAnalyzer()
-#     sentiment_vector = pd.DataFrame()
-#     sentiment_vector['text'] = user_text
-#     sentiment_vector['scores'] = sentiment_vector.text.apply(lambda Text: sid.polarity_scores(Text))
-#     sentiment_vector['pos']  = sentiment_vector['scores'].apply(lambda score_dict: score_dict['pos'])
-#     sentiment_vector['neg']  = sentiment_vector['scores'].apply(lambda score_dict: score_dict['neg'])
-#     sentiment_vector['compound']  = sentiment_vector['scores'].apply(lambda score_dict: score_dict['compound'])
-#     sentiment_vector.drop(columns=['text','scores'],inplace=True)
-    
-#     attributes = pd.concat([sentiment_vector,word_count],axis=1)
-#     attributes = pd.concat([attributes,pd.DataFrame(tt1)],axis=1)
-
-    
-#     overall = lm.predict(attributes)
-#     aroma = lm_aroma.predict(attributes)
-#     acidity = lm_acidity.predict(attributes)
-#     aftertaste = lm_aftertaste.predict(attributes)
-#     flavor = lm_flavor.predict(attributes)
-#     body = lm_body.predict(attributes)
-
-#     if user_coffee_description == '':
-#         st.write('Excited to predict the score of your coffee!')
-#     else:
-#         st.write('Based on your input coffee, I predict it to receive a score of:',overall[0].round(2),'\n\n',
-#                 'An aroma score of (out of 10):',aroma[0].round(2),'\n\n',
-#                 'An acidity score of (out of 10):',acidity[0].round(2),'\n\n',
-#                 'An aftertaste score of (out of 10):',aftertaste[0].round(2),'\n\n',
-#                 'A flavor score of (out of 10):',flavor[0].round(2),'\n\n',
-#                 'A body score of (out of 10):',body[0].round(2))
-
-        
-
-# # Third checkbox: coffee scorer if you know the underlying category scores...
-# if st.checkbox('Predict overall score for a coffee based on its subcategory scores'):
-#     aroma = st.slider('aroma',min_value=1,max_value=10,step=1)
-#     body = st.slider('body',min_value=1,max_value=10,step=1)
-#     flavor = st.slider('flavor',min_value=1,max_value=10,step=1)
-#     aftertaste = st.slider('aftertaste',min_value=1,max_value=10,step=1)
-#     acidity = st.slider('acidity',min_value=1,max_value=10,step=1)
-    
-#     features = ['aroma', 'body', 'flavor', 'aftertaste', 'acidity']
-#     df_feat = pd.DataFrame(columns = features)
-#     df_feat.aroma = [aroma]
-#     df_feat.body = [body]
-#     df_feat.flavor = [flavor]
-#     df_feat.aftertaste = [aftertaste]
-#     df_feat.acidity = [acidity]
-    
-#     overall = rfr_num.predict(df_feat)[0].round(2)
-#     st.write('With subcategory scores as shown above, I predict your coffee to be review overall as:',overall)
-
-# # Fourth checkbox: computer generated review for a coffee...
-# if st.checkbox('Generate a review for a coffee based on which group it falls into'): 
-    
-#     first = st.checkbox("Coffee Type: 1")
-#     second = st.checkbox("Coffee Type: 2")
-#     third = st.checkbox("Coffee Type: 3")
-#     fourth = st.checkbox("Coffee Type: 4")
-#     fifth = st.checkbox("Coffee Type: 5")
-#     sixth = st.checkbox("Coffee Type: 6")
-#     seventh = st.checkbox("Coffee Type: 7")
-#     eighth = st.checkbox("Coffee Type: 8")
-#     ninth = st.checkbox("Coffee Type: 9")
-    
-#     def markov_chain(corpus):
-#         #tokenize text into words
-#         words = []
-#         for review in corpus:
-#             words += review.split(' ')
-
-#         # initialize a default dictionary to hold all of the words and next words
-#         word_dict = defaultdict(list)
-
-#         # create a zipped list of all of the word pairs and put them in word: list of next words format
-#         for first, second in list(zip(words,words[1:])):
-#             word_dict[first].append(second)
-
-#         return dict(word_dict)
-
-#     def generate_review(corpus, n_words=50):
-#         start = random.choice(list(corpus.keys())).capitalize()
-#         sentence = []
-#         sentence.append(start)
-#         for _ in range(n_words):
-#             next_word = random.choice(list(corpus[sentence[-1].lower()]))
-#             sentence.append(next_word)
-
-#         return ' '.join(sentence)+'.'  
-    
-#     text_list = []
-#     if first:
-#         text_list = text_list + [review.lower() for review in generating_reviews[generating_reviews.group == 0].review]
-#     elif second:
-#         text_list = text_list + [review.lower() for review in generating_reviews[generating_reviews.group == 1].review]
-#     elif third:
-#         text_list = text_list + [review.lower() for review in generating_reviews[generating_reviews.group == 2].review]
-#     elif fourth:
-#         text_list = text_list + [review.lower() for review in generating_reviews[generating_reviews.group == 3].review]
-#     elif fifth:
-#         text_list = text_list + [review.lower() for review in generating_reviews[generating_reviews.group == 4].review]
-#     elif sixth:
-#         text_list = text_list + [review.lower() for review in generating_reviews[generating_reviews.group == 5].review]
-#     elif seventh:
-#         text_list = text_list + [review.lower() for review in generating_reviews[generating_reviews.group == 6].review]
-#     elif eighth:
-#         text_list = text_list + [review.lower() for review in generating_reviews[generating_reviews.group == 7].review]
-#     elif ninth:
-#         text_list = text_list + [review.lower() for review in generating_reviews[generating_reviews.group == 8].review]
-
-#     if text_list == []:
-#         st.write('Pick a coffee type to see a computer generated review!')
-#     else:
-#         st.write(generate_review(markov_chain(text_list)))
-
-
-# In[ ]:
-
-
-
-
