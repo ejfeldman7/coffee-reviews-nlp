4d61d710cce311e72c34ff5ebc1d7ab4efd9db2f
Ethan Feldman <ejfeldman7@gmail.com>
2024-02-05 18:37:05 -0800

    Update web_app.py

--- web_app/web_app.py
+++ web_app/web_app.py
@@ -15,8 +15,8 @@ from nltk.sentiment.vader import SentimentIntensityAnalyzer
 
 download('vader_lexicon')
 CUR_DIR = os.path.dirname(os.path.abspath(__file__))
-pickle_jar = os.path.join(CUR_DIR, 'web_app/model_files')
-data_path = os.path.join(CUR_DIR, 'web_app/data')
+pickle_jar = os.path.join(CUR_DIR, 'model_files')
+data_path = os.path.join(CUR_DIR, 'data')
 data = {'df_full': None, 'df_topic_breakdown': None, 'coffee_ratings': None}
 pickles = {'coffee_words': None, 'lm_acidity': None, 'lm_aftertaste': None,
            'lm_flavor': None, 'lm_body': None, 'lm_aroma': None, 'words_to_score_linear': None,
